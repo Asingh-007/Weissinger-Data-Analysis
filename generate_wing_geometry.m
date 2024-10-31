52f0be33bb13326078cc37953fd900e6ff0a0e96
Aniket Singh <107432932+Asingh-007@users.noreply.github.com>
2024-10-30 19:21:50 -0500

    Feat: Mostly Functional Wing Geometry

--- generate_wing_geometry.m
+++ generate_wing_geometry.m
@@ -12,7 +12,7 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
     %% Chord Distribution
 
-    if elliptical_planform
+    if elliptical_planform ~= 1
 
         for i = 1:panel_number
             
@@ -23,6 +23,7 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
             else % Right Wing
                 
                 chord(i) = ((tip_chord - root_chord) / (wing_span/2)) * y_control(i) + root_chord;
+              
              
             end
            
@@ -32,7 +33,8 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
         for i = 1:panel_number 
 
-            chord(i) = root_chord * sqrt((1 - (y_control(i)) / (wing_span / 2))^2);
+            chord(i) = root_chord * sqrt((1 - (y_control(i) / (wing_span / 2))^2));
+           
 
         end
 
@@ -45,11 +47,11 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
     for i = 1:panel_number/2
 
-        twist(i) = deg2rad((-2 * geo_twist_angle / wing_span * y_control(i))); % Compute Twist for Left Wing
+        twist(i) = deg2rad((-2 * geo_twist_angle / wing_span) * y_control(i)); % Compute Twist for Left Wing
 
     end
 
-    twist((panel_number/2 + 1):panel_number) = flip(twist(1:panel_number/2)); %Flip for Right Wing
+    twist(((panel_number/2) + 1):panel_number) = flip(twist(1:panel_number/2)); %Flip for Right Wing
 
 
     %% Airfoil Twist
@@ -58,13 +60,13 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
     for i = ((panel_number/2)+1):panel_number % Airfoil Twist for Right Wing
 
-        aoa_0_dist(i) = (aoa_tip_0 - aoa_root_0) / (wing_span / 2) * y_control(i) + aoa_root_0;
+        aoa_0_dist(i) = ((aoa_tip_0 - aoa_root_0) / (wing_span / 2)) * y_control(i) + aoa_root_0;
 
     end
 
     if flap_percent > 0 
 
-        for i = round((panel_number/2 + 1) + (panel_number/4) * (1 - flap_percent)):round(panel_number - (panel_number/4) * (1 - flap_percent)) % Account for Flap Angle
+        for i = round(((panel_number/2) + 1) + (panel_number/4) * (1 - (flap_percent * 0.01))):round(panel_number - (panel_number/4) * (1 - (flap_percent * 0.01))) % Account for Flap Angle
 
             aoa_0_dist(i) = aoa_0_dist(i) - flap_angle;
 
@@ -72,7 +74,7 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
       
     end
 
-    aoa_0_dist(1:panel_number/2) = flip(aoa_0_dist((panel_number/2)+1:panel_number)); % Flip for Left Wing
+    aoa_0_dist(1:panel_number/2) = flip(aoa_0_dist(((panel_number/2)+1):panel_number)); % Flip for Left Wing
 
 
     
@@ -83,9 +85,9 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
     % Vortex Positions at the Middle of the Wing
     x_vortex_1(panel_number/2+1) = 1/4 * root_chord; 
-    x_vortex_1(panel_number/2+1) = x_vortex_1(panel_number/2+1) + (wing_span/panel_number) * tan(deg2rad(sweep_angle));
+    x_vortex_2(panel_number/2+1) = x_vortex_1(panel_number/2+1) + (wing_span/panel_number) * tan(deg2rad(sweep_angle));
 
-    for i = (panel_number/2+2): panel_number % X Vortex Positions for Right Wing
+    for i = ((panel_number/2)+2):panel_number % X Vortex Positions for Right Wing
 
         x_vortex_1(i) = x_vortex_1(i-1) + (wing_span/panel_number) * tan(deg2rad(sweep_angle));
         x_vortex_2(i) = x_vortex_2(i-1) + (wing_span/panel_number) * tan(deg2rad(sweep_angle));
@@ -93,8 +95,8 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
     end
 
     % Flip for Left Wing
-    x_vortex_1(1:panel_number/2) = flip(x_vortex_2((panel_number/2)+1:panel_number));  
-    x_vortex_2(1:panel_number/2) = flip(x_vortex_1((panel_number/2)+1:panel_number));
+    x_vortex_1(1:panel_number/2) = flip(x_vortex_2(panel_number/2+1:panel_number));  
+    x_vortex_2(1:panel_number/2) = flip(x_vortex_1(panel_number/2+1:panel_number));
 
     
     %% Y Vortex Positions
@@ -113,8 +115,8 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
     end
 
     % Flip for the Right Wing
-    y_vortex_1((panel_number/2+1):panel_number) = -flip(y_vortex_2(1:(panel_number/2)));
-    y_vortex_2((panel_number/2+1):panel_number) = -flip(y_vortex_1(1:(panel_number/2)));
+    y_vortex_1((panel_number/2+1):panel_number) = -flip(y_vortex_2(1:panel_number/2));
+    y_vortex_2((panel_number/2+1):panel_number) = -flip(y_vortex_1(1:panel_number/2));
 
 
     %% X Control Points
@@ -161,8 +163,8 @@ function[x_vortex_1, y_vortex_1, z_vortex_1,x_vortex_2, y_vortex_2, z_vortex_2, 
 
     end
 
-   z_vortex_1(1:panel_number/2) = flip(z_vortex_2((panel_number/2+1):panel_number));
-   z_vortex_2(1:panel_number/2) = flip(z_vortex_1((panel_number/2+1):panel_number));
-   z_control(1:panel_number/2) = flip(z_control((panel_number/2+1):panel_number));
+   z_vortex_1(1:panel_number/2) = flip(z_vortex_2(panel_number/2+1:panel_number));
+   z_vortex_2(1:panel_number/2) = flip(z_vortex_1(panel_number/2+1:panel_number));
+   z_control(1:panel_number/2) = flip(z_control(panel_number/2+1:panel_number));
 
 end

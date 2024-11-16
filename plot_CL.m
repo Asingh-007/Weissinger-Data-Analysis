dc10bba3008fa7c128328e02e7867591f68aad6b
Aniket Singh <107432932+Asingh-007@users.noreply.github.com>
2024-11-16 13:34:19 -0600

    feat: plot aoa all

--- plot_CL.m
+++ plot_CL.m
@@ -1,8 +1,10 @@
-function [cl_aoa] = plot_CL(y_control, lift_distribution, cl_distribution, total_cl_distribution, aoa_vector, freestream_velocity, wing_reference_area, wing_span, panel_number, array_index, wing_name, aoa_index, export_lift)
+function [cl_aoa] = plot_CL(y_control, lift_distribution, cl_distribution, total_cl_distribution, aoa_vector, freestream_velocity, wing_reference_area, wing_span, panel_number, array_index, wing_name, aoa_index, export_lift, plot_aoa_all)
 
 
 % Prandt Lifting Line Theory 
 elliptic_lift = cell(1, size(aoa_vector,2));
+
+cl_aoa = (total_cl_distribution(end) - total_cl_distribution(1)) / (aoa_vector(end) - aoa_vector(1));
 
 for i = 1:size(aoa_vector, 2)
 
@@ -19,38 +21,80 @@ for i = 1:size(aoa_vector, 2)
 
 end
 
-f = figure;
-grid;
-plot(y_control, cl_distribution{array_index}, 'k');
-xlabel('Span (m)');
-ylabel('C_L');
-cl_distribution_title =  append(['C_L Distribution of ', wing_name, ' at ', num2str(aoa_index), '° Angle of Attack']);
-title(cl_distribution_title);
+if plot_aoa_all
 
-
-g = figure;
-grid;
-plot(y_control, lift_distribution{array_index}, 'k', y_control, elliptic_lift{array_index}, '--r');
-xlabel('Span (m)');
-ylabel('Lift')
-legend('Lift Distribution', 'Ideal Elliptic Lift Distribution')
-lift_distribution_title =  append(['Lift and Ideal Elliptic Distribution of ', wing_name, ' at ', num2str(aoa_index), '° Angle of Attack']);
-title(lift_distribution_title);
-
-cl_aoa = (total_cl_distribution(end) - total_cl_distribution(1)) / (aoa_vector(end) - aoa_vector(1));
-
-if size(aoa_vector,2) > 1
+    f = figure(3); 
+    g = figure(4);
+    h = figure(5);
     
-    h = figure;
-    grid;
-    plot(aoa_vector, total_cl_distribution);
-    xlabel('Angle of Attack [°]')
-    ylabel('C_L')
-    legend(['Total Wing Coefficent of Lift = ', num2str(cl_aoa)]);
-    cl_aoa_title =  append(['Coefficent of Lift of ', wing_name, ' over ', num2str(aoa_index), '° Angle of Attack']);
-    title(cl_aoa_title);
+    for i = 1:size(aoa_vector, 2)
 
-     
+        figure(3); 
+        hold on
+        grid;
+        plot(y_control, cl_distribution{i}, 'k');
+        xlabel('Span (m)');
+        ylabel('C_L');
+        cl_distribution_title =  append(['C_L Distribution of ', wing_name]);
+        title(cl_distribution_title);
+        
+        figure(4);
+        hold on
+        grid;
+        plot(y_control, lift_distribution{i}, 'k', y_control, elliptic_lift{i}, '--r');
+        xlabel('Span (m)');
+        ylabel('Lift')
+        legend('Lift Distribution', 'Ideal Elliptic Lift Distribution')
+        lift_distribution_title =  append(['Lift and Ideal Elliptic Distribution of ', wing_name]);
+        title(lift_distribution_title);
+        
+        if size(aoa_vector,2) > 1
+            
+            figure(5);
+            hold on
+            grid;
+            plot(aoa_vector, total_cl_distribution);
+            xlabel('Angle of Attack [°]')
+            ylabel('C_L')
+            legend(['Total Wing Coefficent of Lift = ', num2str(cl_aoa)]);
+            cl_aoa_title =  append(['Coefficent of Lift of ', wing_name, ' over Angle of Attack']);
+            title(cl_aoa_title);
+
+        end
+
+    end
+
+else    
+
+    f = figure;
+    grid;
+    plot(y_control, cl_distribution{array_index}, 'k');
+    xlabel('Span (m)');
+    ylabel('C_L');
+    cl_distribution_title =  append(['C_L Distribution of ', wing_name, ' at ', num2str(aoa_index), '° Angle of Attack']);
+    title(cl_distribution_title);
+    
+    g = figure;
+    grid;
+    plot(y_control, lift_distribution{array_index}, 'k', y_control, elliptic_lift{array_index}, '--r');
+    xlabel('Span (m)');
+    ylabel('Lift')
+    legend('Lift Distribution', 'Ideal Elliptic Lift Distribution')
+    lift_distribution_title =  append(['Lift and Ideal Elliptic Distribution of ', wing_name, ' at ', num2str(aoa_index), '° Angle of Attack']);
+    title(lift_distribution_title);
+    
+    if size(aoa_vector,2) > 1
+        
+        h = figure;
+        grid;
+        plot(aoa_vector, total_cl_distribution);
+        xlabel('Angle of Attack [°]')
+        ylabel('C_L')
+        legend(['Total Wing Coefficent of Lift = ', num2str(cl_aoa)]);
+        cl_aoa_title =  append(['Coefficent of Lift of ', wing_name, ' over ', num2str(aoa_index), '° Angle of Attack']);
+        title(cl_aoa_title);
+    
+    end
 
 end
 
@@ -60,7 +104,8 @@ if export_lift
 
     exportgraphics(g,  strcat(['Output\', lift_distribution_title, '_Plot.png']));
 
-   
+    exportgraphics(h,  strcat(['Output\', cl_aoa_title, '_Plot.png']));
+
 end
 
 

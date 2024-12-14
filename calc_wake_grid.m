e8bfdfc034ba9fb5024cea1ab559cceaa2ce857b
Aniket Singh <107432932+Asingh-007@users.noreply.github.com>
2024-12-14 17:23:27 -0600

    feat: plot wake

---
+++ calc_wake_grid.m
@@ -0,0 +1,42 @@
+function[x_velocity, y_velocity, z_velocity] = calc_wake_grid(x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, x_wake, y_mesh, z_mesh, panel_number, aoa, aoa_0_dist, wake_alignment, circulation, freestream_velocity)
+
+LHS_wake_drag_matrix_x = zeros(1,panel_number);
+LHS_wake_drag_matrix_y = LHS_wake_drag_matrix_x;
+LHS_wake_drag_matrix_z = LHS_wake_drag_matrix_x;
+
+if wake_alignment == WakeAlignment.Freestream
+
+    freestream_velocity_vector = [cosd(aoa - aoa_0_dist(panel_number/2)); 0 ; sind(aoa - aoa_0_dist(panel_number/2))];
+
+else
+
+    freestream_velocity_vector = [1; 0 ; 0];
+
+end
+
+x_horseshoe = cell(panel_number, panel_number);
+LHS_wake_drag_matrix = x_horseshoe;
+y_horseshoe_1 = x_horseshoe;
+y_horseshoe_2 = x_horseshoe;
+
+for i = 1:panel_number
+
+    rx_horseshoe = [(x_vortex_2(i) - x_vortex_1(i)); (y_vortex_2(i) - y_vortex_1(i)); (z_vortex_2(i) - z_vortex_1(i))];
+    r_bound_1 = [(x_wake(1) - x_vortex_1(i)); (y_mesh(1) - y_vortex_1(i)); (z_mesh(1) - z_vortex_1(i))];
+    r_bound_2 = [(x_wake(1) - x_vortex_2(i)); (y_mesh(1) - y_vortex_2(i)); (z_mesh(1) - z_vortex_2(i))];
+
+    x_horseshoe{1,i} = (cross(r_bound_1, r_bound_2) / norm(cross(r_bound_1, r_bound_2))^2)...
+        * (dot(rx_horseshoe, ((r_bound_1 / norm(r_bound_1)) - (r_bound_2 / norm(r_bound_2)))));
+    y_horseshoe_1{1,i} = cross(freestream_velocity_vector, r_bound_1) / (norm(r_bound_1) * (norm(r_bound_1) - dot(freestream_velocity_vector, r_bound_1)));
+    y_horseshoe_2{1,i} = cross(freestream_velocity_vector, r_bound_2) / (norm(r_bound_2) * (norm(r_bound_2) - dot(freestream_velocity_vector, r_bound_2)));
+
+    LHS_wake_drag_matrix{1,i} = (1/(4*pi)) * (x_horseshoe{1,i} - y_horseshoe_1{1,i} + y_horseshoe_2{1,i});
+    LHS_wake_drag_matrix_x(1,i) = LHS_wake_drag_matrix{1,i}(1);
+    LHS_wake_drag_matrix_y(1,i) = LHS_wake_drag_matrix{1,i}(2);
+    LHS_wake_drag_matrix_z(1,i) = LHS_wake_drag_matrix{1,i}(3);
+
+end
+
+x_velocity = LHS_wake_drag_matrix_x * circulation + freestream_velocity * cosd(aoa - aoa_0_dist(1));
+y_velocity = LHS_wake_drag_matrix_y * circulation;
+z_velocity = LHS_wake_drag_matrix_z * circulation + freestream_velocity * sind(aoa - aoa_0_dist(1));
---
+++ plot_wake_master.m
@@ -0,0 +1,67 @@
+
+
+function[ x_mesh, y_mesh, z_mesh] = plot_wake_master(x_grid_number, y_grid_number, z_grid_number, wing_span, wake_alignment, aoa_vector, x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, panel_number, aoa_0_dist, circulation, freestream_velocity, aoa_index, array_index, wing_name, export_name)
+
+f = figure;
+
+x_wake = 10 * wing_span;
+
+if wake_alignment == WakeAlignment.Freestream
+
+    z_interval = [(-wing_span/8 + x_wake * sind(max(aoa_vector))); (wing_span/8 + x_wake * sind(max(aoa_vector)))];
+
+else
+
+    z_interval = [-wing_span/8; wing_span/8];
+
+end
+
+y_interval = [(-1.2 * wing_span/2); (1.2 * wing_span/2)];
+
+x_interval = [0 ; x_wake/8];
+
+y_grid = linspace(y_interval(1), y_interval(2), y_grid_number)';
+z_grid = linspace(z_interval(1), z_interval(2), z_grid_number)';
+x_grid = linspace(x_interval(1), x_interval(2), x_grid_number)';
+[x_mesh, y_mesh, z_mesh] = meshgrid(x_grid, y_grid, z_grid);
+
+    for j = 1:y_grid_number
+
+        for k = 1:z_grid_number
+
+            for w = 1:x_grid_number
+
+                [x_velocity(j,k,w), y_velocity(j,k,w), z_velocity(j,k,w)] = calc_wake_grid(x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, x_mesh(j,k,w), y_mesh(j,k,w), z_mesh(j,k,w), panel_number, aoa_vector(array_index), aoa_0_dist, wake_alignment, circulation{array_index}, freestream_velocity);
+
+            end
+
+        end
+
+    end
+
+   hold on;
+   wake_title = append('Wake Plot of ', wing_name, ' at ', num2str(aoa_index), '° Angle of Attack');
+   title(wake_title);
+   quiverC3D(x_mesh, y_mesh, z_mesh, x_velocity, y_velocity, z_velocity, 2);
+   view(90,0);
+   xlabel('X');
+   ylabel('Y');
+   zlabel('Z');
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

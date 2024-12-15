6e44717a7b1f9f39fbff2f7758b868448087e439
Aniket Singh <107432932+Asingh-007@users.noreply.github.com>
2024-12-14 18:20:53 -0600

    fix: uniform grid number

--- plot_wake_master.m
+++ plot_wake_master.m
@@ -1,6 +1,6 @@
 
 
-function[ x_mesh, y_mesh, z_mesh] = plot_wake_master(x_grid_number, y_grid_number, z_grid_number, wing_span, wake_alignment, aoa_vector, x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, panel_number, aoa_0_dist, circulation, freestream_velocity, aoa_index, array_index, wing_name, export_name, export_wake)
+function[ x_mesh, y_mesh, z_mesh] = plot_wake_master(grid_number, wing_span, wake_alignment, aoa_vector, x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, panel_number, aoa_0_dist, circulation, freestream_velocity, aoa_index, array_index, wing_name, export_name, export_wake)
 
 f = figure;
 
@@ -20,16 +20,16 @@ y_interval = [(-1.2 * wing_span/2); (1.2 * wing_span/2)];
 
 x_interval = [-x_wake/64 ; x_wake/8];
 
-y_grid = linspace(y_interval(1), y_interval(2), y_grid_number)';
-z_grid = linspace(z_interval(1), z_interval(2), z_grid_number)';
-x_grid = linspace(x_interval(1), x_interval(2), x_grid_number)';
+y_grid = linspace(y_interval(1), y_interval(2), grid_number)';
+z_grid = linspace(z_interval(1), z_interval(2), grid_number)';
+x_grid = linspace(x_interval(1), x_interval(2), grid_number)';
 [x_mesh, y_mesh, z_mesh] = meshgrid(x_grid, y_grid, z_grid);
 
-    for j = 1:y_grid_number
+    for j = 1:grid_number
 
-        for k = 1:z_grid_number
+        for k = 1:grid_number
 
-            for w = 1:x_grid_number
+            for w = 1:grid_number
 
                 [x_velocity(j,k,w), y_velocity(j,k,w), z_velocity(j,k,w)] = calc_wake_grid(x_vortex_1, x_vortex_2, y_vortex_1, y_vortex_2, z_vortex_1, z_vortex_2, x_mesh(j,k,w), y_mesh(j,k,w), z_mesh(j,k,w), panel_number, aoa_vector(array_index), aoa_0_dist, wake_alignment, circulation{array_index}, freestream_velocity);
 

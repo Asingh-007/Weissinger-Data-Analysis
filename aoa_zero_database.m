ec4eaf87703a01449db2f3b9a7b66b1f078b6935
Aniket Singh <107432932+Asingh-007@users.noreply.github.com>
2024-12-31 12:10:05 -0600

    feat: reynolds number interpolation and database rework

--- aoa_zero_database.m
+++ aoa_zero_database.m
@@ -1,13 +1,10 @@
 
 function[aoa_0, airfoil_name] = aoa_zero_database(af_data_name, airfoil_database_path)
 
-    airfoil_database = readtable(airfoil_database_path);
+    airfoil_database = readtable(airfoil_database_path,'Sheet',af_data_name);
 
-    row_number = find(strcmp(airfoil_database.Airfoil, af_data_name));
-
-    aoa_0 = airfoil_database{row_number, 'AOA_Zero'}; 
+    aoa_0 = rmmissing(airfoil_database.AOA_Zero);
 
     airfoil_name = af_data_name;
-
 
 end
